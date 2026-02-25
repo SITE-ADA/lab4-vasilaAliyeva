8effc30cb0a0c5b157065195db18bd37ca7528ce
vasilaAliyeva <vasilaaliyeffa@gmail.com>
2026-02-25 12:36:46 +0400

    Add ProductRepositoryImpl implementation

---
+++ src/main/java/az/edu/ada/wm2/lab4/repository/ProductRepositoryImpl.java
@@ -0,0 +1,45 @@
+package az.edu.ada.wm2.lab4.repository;
+
+import az.edu.ada.wm2.lab4.model.Product;
+import org.springframework.stereotype.Repository;
+
+import java.util.*;
+
+@Repository
+public class ProductRepositoryImpl implements ProductRepository {
+
+    private final Map<UUID, Product> products = new HashMap<UUID, Product>();
+
+    @Override
+    public Product save(Product product) {
+        if (product.getId() == null) {
+            UUID uuid = UUID.randomUUID();
+            product = new Product(
+                    uuid, product.getProductName(),
+                    product.getPrice(), product.getExpirationDate()
+            );
+        }
+        products.put(product.getId(), product);
+        return product;
+    }
+
+    @Override
+    public Optional<Product> findById(UUID id) {
+        return Optional.ofNullable(products.get(id));
+    }
+
+    @Override
+    public List<Product> findAll() {
+        return new ArrayList<>(products.values());
+    }
+
+    @Override
+    public void deleteById(UUID id) {
+        products.remove(id);
+    }
+
+    @Override
+    public boolean existsById(UUID id) {
+        return products.containsKey(id);
+    }
+}
